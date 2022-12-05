ac4a53167aa3ec0e4e5966f1a49230bb53437226
Renato Ribeiro <renato.ribeiro@sky.uk>
2022-12-05 18:43:36 +0000

    Utilities improvements
    - Add translatesAutoresizingMaskIntoConstraints(_:) to Collection<.Element: UIView>

--- Utilities/Extensions/Collection+Utils.swift
+++ Utilities/Extensions/Collection+Utils.swift
@@ -1,5 +1,13 @@
-import Foundation
+import UIKit
 
 public extension Collection {
     var isNotEmpty: Bool { !self.isEmpty }
+}
+
+public extension Collection where Element: UIView {
+    func translateAutoresizingMaskIntoConstraints(_ flag: Bool) {
+        self.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = flag
+        }
+    }
 }
